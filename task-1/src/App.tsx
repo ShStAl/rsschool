b9710d474ba6ae2e76cb956cdf6b0d750a806b08
Stanislav Shevelev <128316028+ShStAl@users.noreply.github.com>
2024-07-07 20:53:21 +0300

    refactor: rebuild App with class component

--- task-1/src/App.tsx
+++ task-1/src/App.tsx
@@ -1,23 +1,26 @@
 import './App.css'
+import { Component } from 'react'
 
-function App() {
+class App extends Component {
 
-    return (
-        <>
-            <div className="layout">
-                <div className="top-section">
-                    <div className="search-bar">
-                        <input className="search-input" placeholder="Type search here..." />
-                        <button className="search-btn">
-                            Search
-                        </button>
+    render() {
+        return (
+            <>
+                <div className="layout">
+                    <div className="top-section">
+                        <div className="search-bar">
+                            <input className="search-input" placeholder="Type search here..." />
+                            <button className="search-btn">
+                                Search
+                            </button>
+                        </div>
+                    </div>
+                    <div className="bottom-section">
                     </div>
                 </div>
-                <div className="bottom-section">
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default App
