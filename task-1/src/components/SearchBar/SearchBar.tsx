806af3de8ff6e32329a043118ceb679f634cc397
Stanislav Shevelev <128316028+ShStAl@users.noreply.github.com>
2024-07-15 00:11:33 +0300

    refactor: rewrite SearchBar component

--- task-1/src/components/SearchBar/SearchBar.tsx
+++ task-1/src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent } from 'react'
+import { Component } from 'react'
 
 interface SearchBarProps {
     searchTerm: string
@@ -7,25 +8,45 @@ interface SearchBarProps {
     onThrowErrorClick: () => void
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({
-    searchTerm,
-    onSearchInputChange,
-    onSearchButtonClick,
-    onThrowErrorClick,
-}) => (
-    <div className="search-bar">
-        <input
-            className="search-input"
-            placeholder="Type product name here..."
-            type="text"
-            value={searchTerm}
-            onChange={onSearchInputChange}
-        />
-        <button className="search-btn" onClick={onSearchButtonClick}>
-            Search
-        </button>
-        <button onClick={onThrowErrorClick}>Throw Error</button>
-    </div>
-)
+class SearchBar extends Component<SearchBarProps> {
+    constructor(props: SearchBarProps) {
+        super(props);
+        this.state = {
+            error: false,
+        };
+    }
+
+    componentDidUpdate() {
+        if (this.state.error) {
+            throw new Error('Error happened!');
+        }
+    }
+
+    handleThrowErrorClick = () => {
+        this.setState({
+            error: true,
+        });
+    };
+
+    render() {
+        const { searchTerm, onSearchInputChange, onSearchButtonClick, onThrowErrorClick } = this.props;
+
+        return (
+            <div className="search-bar">
+                <input
+                    className="search-input"
+                    placeholder="Type product name here..."
+                    type="text"
+                    value={searchTerm}
+                    onChange={onSearchInputChange}
+                />
+                <button className="search-btn" onClick={onSearchButtonClick}>
+                    Search
+                </button>
+                <button onClick={this.handleThrowErrorClick}>Throw Error</button>
+            </div>
+        );
+    }
+}
 
 export default SearchBar
