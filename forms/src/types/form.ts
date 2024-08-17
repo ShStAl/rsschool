2bfe7e10e6097e0e3743075fab1f431fc41b058b
Stanislav Shevelev <128316028+ShStAl@users.noreply.github.com>
2024-08-17 20:38:31 +0300

    feat: create validation schema

---
+++ forms/src/types/form.ts
@@ -0,0 +1,11 @@
+export interface FormData {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  passwordConfirm?: string;
+  gender: string;
+  terms: boolean;
+  image: string;
+  country: string;
+}
