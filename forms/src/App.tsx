bf08138341785816cde7c8f55474d32649e3dbb1
Stanislav Shevelev <128316028+ShStAl@users.noreply.github.com>
2024-08-17 16:40:44 +0300

    feat: add prettier and husky

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,12 +1,16 @@
 function App() {
+  function x({}: any) {
+    return 1;
+  }
 
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </>
-    )
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        {x({})}
+      </h1>
+    </>
+  );
 }
 
-export default App
+export default App;
